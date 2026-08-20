6f3eea9f818fe31b7ad0b6d3a59b24e66d81d4ca
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-08-19 19:35:47 -0700

    Add: merge-intervals - submission-19

---
+++ Data Structures & Algorithms/merge-intervals/submission-19.py
@@ -0,0 +1,54 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+
+        ans = []
+
+        for interval in intervals:
+            if not ans or interval[0] > ans[-1][1]:
+                ans.append(interval)
+            else:
+                ans[-1] = [
+                    min(ans[-1][0], interval[0]),
+                    max(ans[-1][1], interval[1])
+                ]
+
+        return ans
+        # sortedIntervals = sorted(intervals, key=lambda x:x[0], reverse=False)
+        # print(sortedIntervals)
+
+        # ans = []
+
+        # for i in sortedIntervals:
+        #     length = len(ans)
+            
+        #     if length == 0 or i[0] > ans[length-1][1]:
+        #         ans.append(i)     
+        #     else:
+        #         j = length-1
+        #         tempInterval = i
+                
+        #         while j!=0 and tempInterval[0] <= ans[j][1]:
+                  
+        #             if not (tempInterval[1] < ans[j][0]):
+        #                 tempInterval = [
+        #                     min(tempInterval[0],ans[j][0]), 
+        #                     max(tempInterval[1],ans[j][1])]
+        #                 ans.pop()
+
+        #             j-=1 
+
+        #         if j != 0:
+        #             ans.append(tempInterval)
+
+        #         if j == 0:
+        #             if tempInterval[1] < ans[j][0]:
+        #                 ans.insert(0,tempInterval)
+        #             elif tempInterval[0] > ans[j][1]:
+        #                 ans.insert(1,tempInterval)
+        #             else:
+        #                 ans[0] = [
+        #                     min(tempInterval[0],ans[0][0]), 
+        #                     max(tempInterval[1],ans[0][1])]
+
+        # return ans
