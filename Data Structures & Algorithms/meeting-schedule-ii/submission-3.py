95766f13d5b5ef49e12015af3be24630c951c988
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-08-21 14:35:43 -0700

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,46 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+
+        startTimes, endTimes = [], []
+        for i in intervals:
+            startTimes.append(i.start)
+            endTimes.append(i.end)
+
+        heapq.heapify(startTimes)
+        heapq.heapify(endTimes)
+
+        rooms, maxRooms = 0, 0
+
+        while len(startTimes) > 0 and len(endTimes) > 0:
+
+            if startTimes[0] < endTimes[0]:
+                heapq.heappop(startTimes)
+                rooms += 1
+            elif startTimes[0] == endTimes[0]:
+                heapq.heappop(startTimes)
+                heapq.heappop(endTimes)
+            else:
+                heapq.heappop(endTimes)
+                rooms -= 1
+
+            maxRooms = max(maxRooms, rooms)
+
+        # while len(startTimes) > 0:
+        #     startTimes.pop(0)
+        #     rooms += 1
+
+        #     maxRooms = max(maxRooms, rooms)
+        
+        return maxRooms
+
+
+        
+        
