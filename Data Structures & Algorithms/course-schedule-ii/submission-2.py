76fcc0068aaaa2cc1285e0860b68420f12abfd51
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-08-28 22:09:12 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        matrix = defaultdict(list)
+        indegrees = [0] * numCourses
+
+        for i in prerequisites:
+            a,b = i
+            matrix[b].append(a)
+            indegrees[a] += 1
+        
+        queue = deque()
+
+        for i in range(0,len(indegrees)):
+            if indegrees[i] == 0:
+                queue.append(i)
+
+        counter = 0
+        ans = []
+
+        while len(queue)>0:
+            course = queue.popleft()
+            ans.append(course)
+            counter += 1
+
+            for i in matrix[course]:
+                indegrees[i] -= 1
+
+                if indegrees[i] == 0:
+                    queue.append(i)
+
+        if counter == numCourses:
+            return ans
+
+        return []
+
+        
