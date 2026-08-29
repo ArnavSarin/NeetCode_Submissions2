4caa59301f2495bbd6bd91f16d7f6d48c977d80e
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-08-28 22:59:22 -0700

    Add: valid-tree - submission-10

---
+++ Data Structures & Algorithms/valid-tree/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        matrix = defaultdict(list)
+        indegrees = [0]*n
+
+        for i in edges:
+            a,b = i
+            matrix[a].append(b)
+            matrix[b].append(a)
+        
+        queue = deque([(0,[])])
+
+        counter = 0
+
+        while len(queue)>0:
+            node, prev = queue.popleft()
+
+            counter += 1
+
+            for i in matrix[node]:
+                if i in prev[:-1]:
+                    return False
+                if len(prev)==0 or i != prev[-1]:
+                    queue.append((i,prev + [node]))
+        
+        if counter != n:
+            return False
+
+        return True
+
+        
